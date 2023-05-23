719e4307d4457e1c1fc53c51065135a9bf2661ae
彭曄澤 <94458936@qq.com>
2023-05-23 16:06:11 +0800

    boost库升级到1.82

---
+++ Bat/CopyBoost.bat
@@ -0,0 +1,10 @@
+@echo on
+
+xcopy "%BoostInclude%/boost/*.h" "../boost/boost/" /F /Y /E 
+xcopy "%BoostInclude%/boost/*.hpp" "../boost/boost/" /F /Y /E 
+
+xcopy "%BoostLib%/*chrono*" "../boost/stage/lib/" /F /Y  
+xcopy "%BoostLib%/*time*" "../boost/stage/lib/" /F /Y 
+xcopy "%BoostLib%/*thread*" "../boost/stage/lib/" /F /Y 
+
+pause
