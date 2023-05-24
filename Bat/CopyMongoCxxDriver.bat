59fa778b171c814e0294fe50068b8b07e6695e32
彭曄澤 <94458936@qq.com>
2023-05-24 20:04:12 +0800

    mongo库漏提交

--- Bat/CopyMongoCxxDriver.bat
+++ Bat/CopyMongoCxxDriver.bat
@@ -1,6 +1,6 @@
 @echo on
 
-xcopy "%MongocxxInclude%/*.hpp" "../mongo-cxx-driver/src/mongocxx/" /F /Y /E  
+xcopy "%MongocxxInclude%/*.hpp" "../mongo-cxx-driver/src/" /F /Y /E  
 
 xcopy "%MongoCxxDriverWin32DebugLib%/*.lib" "../mongo-cxx-driver/BuildWin32/src/mongocxx/Debug/" /F /Y   
 xcopy "%MongoCxxDriverWin32DebugLib%/*.dll" "../mongo-cxx-driver/BuildWin32/src/mongocxx/Debug/" /F /Y  
@@ -10,5 +10,8 @@ xcopy "%MongoCxxDriverX64DebugLib%/*.lib" "../mongo-cxx-driver/BuildX64/src/mong
 xcopy "%MongoCxxDriverX64DebugLib%/*.dll" "../mongo-cxx-driver/BuildX64/src/mongocxx/Debug/" /F /Y   
 xcopy "%MongoCxxDriverX64ReleaseLib%/*.lib" "../mongo-cxx-driver/BuildX64/src/mongocxx/Release/" /F /Y  
 xcopy "%MongoCxxDriverX64ReleaseLib%/*.dll" "../mongo-cxx-driver/BuildX64/src/mongocxx/Release/" /F /Y   
+
+xcopy "%MongocxxInclude%/../BuildX64/mongo-c-driver/*.dll" "../mongo-cxx-driver/BuildX64/mongo-c-driver/" /F /Y /E 
+xcopy "%MongocxxInclude%/../BuildWin32/mongo-c-driver/*.dll" "../mongo-cxx-driver/BuildWin32/mongo-c-driver/" /F /Y /E 
 
 pause
