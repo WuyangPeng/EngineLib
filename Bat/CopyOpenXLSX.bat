528814701471331898601eb0003abf55386ce1b7
PengWuyang\pwy <94458936@qq.com>
2023-11-11 10:27:54 +0800

    修正openxlsx目录结构

--- Bat/CopyOpenXLSX.bat
+++ Bat/CopyOpenXLSX.bat
@@ -2,14 +2,14 @@
 
 xcopy "%OpenXLSXInclude%/*.hpp" "../OpenXLSX/OpenXLSX/" /F /Y /E
 
-xcopy "%OpenXLSXWin32DebugLib%/*.lib" "../OpenXLSX/OpenXLSX/BuildWin32/output/Debug/" /F /Y   
-xcopy "%OpenXLSXWin32ReleaseLib%/*.lib" "../OpenXLSX/OpenXLSX/BuildWin32/output/Release/" /F /Y  
-xcopy "%OpenXLSXX64DebugLib%/*.lib" "../OpenXLSX/OpenXLSX/BuildX64/output/Debug/" /F /Y   
-xcopy "%OpenXLSXX64ReleaseLib%/*.lib" "../OpenXLSX/OpenXLSX/BuildX64/output/Release/" /F /Y  
+xcopy "%OpenXLSXWin32DebugLib%/*.lib" "../OpenXLSX/BuildWin32/output/Debug/" /F /Y   
+xcopy "%OpenXLSXWin32ReleaseLib%/*.lib" "../OpenXLSX/BuildWin32/output/Release/" /F /Y  
+xcopy "%OpenXLSXX64DebugLib%/*.lib" "../OpenXLSX/BuildX64/output/Debug/" /F /Y   
+xcopy "%OpenXLSXX64ReleaseLib%/*.lib" "../OpenXLSX/BuildX64/output/Release/" /F /Y  
 
-xcopy "%OpenXLSXWin32DebugLib%/*.dll" "../OpenXLSX/OpenXLSX/BuildWin32/output/Debug/" /F /Y   
-xcopy "%OpenXLSXWin32ReleaseLib%/*.dll" "../OpenXLSX/OpenXLSX/BuildWin32/output/Release/" /F /Y  
-xcopy "%OpenXLSXX64DebugLib%/*.dll" "../OpenXLSX/OpenXLSX/BuildX64/output/Debug/" /F /Y   
-xcopy "%OpenXLSXX64ReleaseLib%/*.dll" "../OpenXLSX/OpenXLSX/BuildX64/output/Release/" /F /Y  
+xcopy "%OpenXLSXWin32DebugLib%/*.dll" "../OpenXLSX/BuildWin32/output/Debug/" /F /Y   
+xcopy "%OpenXLSXWin32ReleaseLib%/*.dll" "../OpenXLSX/BuildWin32/output/Release/" /F /Y  
+xcopy "%OpenXLSXX64DebugLib%/*.dll" "../OpenXLSX/BuildX64/output/Debug/" /F /Y   
+xcopy "%OpenXLSXX64ReleaseLib%/*.dll" "../OpenXLSX/BuildX64/output/Release/" /F /Y  
 
 pause
