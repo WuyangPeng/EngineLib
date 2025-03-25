ad369d3355c7d65d43b6971d3c8a587084c5bb2d
彭武阳 <94458936@qq.com>
2025-03-25 15:49:54 +0800

    + 平台修改

--- ACE/ACE_wrappers/ace/config.h
+++ ACE/ACE_wrappers/ace/config.h
@@ -1,7 +1,11 @@
 #ifndef ACE_CONFIG_H
 #define ACE_CONFIG_H
 
+#if (defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)) && !defined(_CRAYC)
+#include "config-linux.h"
+#else // !defined(linux)
 #include "config-win32.h"
- 
+#endif // defined(linux)
+
 #endif /* ACE_CONFIG_H */
 
