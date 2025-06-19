b3358889d97299b76d11d4a92987598523449c63
彭武阳 <94458936@qq.com>
2025-06-19 18:08:17 +0800

    + 依赖库版本更新

--- Bat/create_lib.sh
+++ Bat/create_lib.sh
@@ -201,8 +201,6 @@ if [ ! -f /data/coding/Libs/activemq_installed.txt ]; then
 	if [ -f /data/coding/Libs/activemq_installed_clone.txt ]; then
 	
 		cd /data/coding/Libs/activemq
-		
-		export AMQCPP_BUILD_DIR="/data/coding/Libs/activemq/build"
 	
 		sh ./build.sh configure
 		./configure
